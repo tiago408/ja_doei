2237996eb5b28452a3183e05e3d370d452009828
tiago408 <tiago@iwm.digital>
2026-09-01 16:55:27 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -4549,13 +4549,13 @@ export default function App() {
                               <option value="">Selecione o tamanho</option>
                               {newSizeType === 'roupa' ? (
                                 <>
-                                  <optgroup label="Infantil / Bebê">
-                                    {CLOTHING_KIDS_SIZE_OPTIONS.map((sizeOption) => (
+                                  <optgroup label="Moda Adulto">
+                                    {CLOTHING_ADULT_SIZE_OPTIONS.map((sizeOption) => (
                                       <option key={sizeOption} value={sizeOption}>{sizeOption}</option>
                                     ))}
                                   </optgroup>
-                                  <optgroup label="Adulto">
-                                    {CLOTHING_ADULT_SIZE_OPTIONS.map((sizeOption) => (
+                                  <optgroup label="Moda Infantil / Bebê">
+                                    {CLOTHING_KIDS_SIZE_OPTIONS.map((sizeOption) => (
                                       <option key={sizeOption} value={sizeOption}>{sizeOption}</option>
                                     ))}
                                   </optgroup>
